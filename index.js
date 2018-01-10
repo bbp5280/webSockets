66f7a1431e1ec7598a5d0cd6bb14f4ea3a228f13
Ben Porter <bbp5280@gmail.com>
2018-01-09 17:49:51 -0700

    chat push

---
+++ index.js
@@ -0,0 +1,29 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', (socket) => {
+    io.emit('logged in', 'User has logged in')
+
+    socket.on('chat message', (message) => {
+        io.emit('chat message', message);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('logged out', 'User Has Left Chat')
+    });
+
+    socket.on('user-typing', message => {
+        io.emit('current-typing', message)
+    });
+
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
